acd622cf25ec7d5fcd9effecc709d11ee7362408
Chris <cfmcginn@mit.edu>
2018-04-30 17:01:00 +0200

    Update scaleErrorTool

--- include/scaleErrorTool.h
+++ include/scaleErrorTool.h
@@ -8,8 +8,14 @@
 #include <fstream>
 
 #include "TMath.h"
+#include "TH2F.h"
+#include "TLatex.h"
+#include "TCanvas.h"
+#include "TStyle.h"
 
 #include "include/doGlobalDebug.h"
+#include "include/checkMakeDir.h"
+#include "include/plotUtilities.h"
 
 class scaleErrorTool{
  public:
@@ -43,15 +49,20 @@ class scaleErrorTool{
   double getSigDataMinusMC(std::string fullLine);
   double getSigDataMinusMCErr(std::string fullLine);
 
+  void makeErrorMaps();
+
   std::string errorFile_;
 
-  std::vector<std::string> centStrV = {"0-10%", "10-30%", "30-50%", "50-100%"};//0 thru 3
+  //  std::vector<std::string> centStrV = {"0-10%", "10-30%", "30-50%", "50-100%"};//0 thru 3
+  std::vector<std::string> centStrV = {"0-5%", "5-10%", "10-20%", "20-30%", "30-40%", "40-50%", "50-70%", "70-100%"};//0 thru 7
   std::vector<std::string> absEtaStrV = {"AbsEta0p0to1p0", "AbsEta1p0to2p0"};//0 thru 1
   std::vector<std::string> rStrV = {"R0p3", "R0p4", "R0p6", "R0p8", "R1p0"}; //0 thru 4
   std::vector<std::string> flowStrV = {"NoFlow", "FlowDefaultInRho"};//0 thru 1
 
-  std::vector<int> centValLowV = {0, 10, 30, 50};//0 thru 3
-  std::vector<int> centValHiV = {10, 30, 50, 90};//0 thru 3
+  //  std::vector<int> centValLowV = {0, 10, 30, 50};//0 thru 3
+  //  std::vector<int> centValHiV = {10, 30, 50, 90};//0 thru 3
+  std::vector<int> centValLowV = {0,  5, 10, 20, 30, 40, 50, 70};//0 thru 7
+  std::vector<int> centValHiV = { 5, 10, 20, 30, 40, 50, 70, 100};//0 thru 7
   std::vector<double> absEtaValLowV = {0., 1.};//0 thru 1
   std::vector<double> absEtaValHiV = {1., 2.};//0 thru 1
   std::vector<int> rValV = {3, 4, 6, 8, 10}; //0 thru 4
@@ -60,6 +71,8 @@ class scaleErrorTool{
   std::map<unsigned int, double> sigDataMinusMC;
   std::map<unsigned int, double> muDataMinusMCErr;
   std::map<unsigned int, double> sigDataMinusMCErr;
+
+  Bool_t initialized = false;
 };
 
 scaleErrorTool::scaleErrorTool()
@@ -78,7 +91,8 @@ bool scaleErrorTool::Init()
 {
   if(errorFile_.size() == 0  || errorFile_.find(".txt") == std::string::npos){
     std::cout << "SCALEERRORTOOL: Error file string \'" << errorFile_ << "\' is invalid. return false" << std::endl;
-    return false;
+    initialized = false;
+    return initialized;
   }
 
   std::ifstream file(errorFile_.c_str());
@@ -114,7 +128,8 @@ bool scaleErrorTool::Init()
 
   file.close();
 
-  return true;
+  initialized = true;
+  return initialized;
 }
 
 
@@ -391,5 +406,139 @@ double scaleErrorTool::getSigDataMinusMCErr(std::string fullLine)
   return sigDataMinusMCErr[key];
 }
 
+
+void scaleErrorTool::makeErrorMaps()
+{
+  if(!initialized){
+    std::cout << "WARNING: scaleErrorTool is not initialized, cannot create error maps. return 1" << std::endl;
+    return;
+  }
+
+  std::cout << "scaleErrorTool is creating error maps..." << std::endl;
+  
+  //  const Int_t nCentBins = 4;
+  //  const Double_t centBins[nCentBins+1] = {0, 10, 30, 50, 100};
+  const Int_t nCentBins = 7;
+  const Double_t centBins[nCentBins+1] = {0, 5, 10, 20, 30, 40, 50, 70};
+  const Int_t nRBins = 5;
+  const Double_t rBins[nRBins+1] = {0.25, 0.35, 0.45, 0.7, 0.9, 1.1};
+  const Double_t rBins2[nRBins] = {0.3, 0.4, 0.6, 0.8, 1.0};
+
+  std::vector<std::string> typeVal = {"mu", "mu", "mu", "mu", "sigma", "sigma", "sigma", "sigma"};
+  std::vector<std::string> etaVal = {"AbsEta0p0to1p0", "AbsEta1p0to2p0", "AbsEta0p0to1p0", "AbsEta1p0to2p0", "AbsEta0p0to1p0", "AbsEta1p0to2p0", "AbsEta0p0to1p0", "AbsEta1p0to2p0"};
+  std::vector<std::string> etaLabel;
+  for(unsigned int etaI = 0; etaI < etaVal.size(); ++etaI){
+    std::string etaStr = etaVal.at(etaI);
+    etaStr.replace(0,6,"");
+    while(etaStr.find("p") != std::string::npos){
+      etaStr.replace(etaStr.find("p"), 1, ".");     
+    }
+    etaStr.replace(etaStr.find("to"), 2, " < |#eta| < ");
+    etaLabel.push_back(etaStr);
+  }
+
+  std::vector<std::string> flowVal = {"NoFlow", "NoFlow", "FlowDefaultInRho", "FlowDefaultInRho", "NoFlow", "NoFlow", "FlowDefaultInRho", "FlowDefaultInRho"};
+  std::vector<std::string> flowLabel;
+  for(unsigned int fI = 0; fI < flowVal.size(); ++fI){
+    if(flowVal.at(fI).find("No") != std::string::npos) flowLabel.push_back("No Flow Corr.");
+    else flowLabel.push_back("Flow Corr.");
+  }
+
+  std::vector<TH2F*> histVect;
+  histVect.reserve(typeVal.size());
+  for(unsigned int hI = 0; hI < typeVal.size(); ++hI){
+    histVect.push_back(NULL);
+    histVect.at(hI) = new TH2F((typeVal.at(hI) + "ErrorMap_" + etaVal.at(hI) + "_" + flowVal.at(hI) + "_h").c_str(), ";Centrality%;Jet or Random Cone R", nCentBins, centBins, nRBins, rBins);
+    histVect.at(hI)->GetXaxis()->CenterTitle();
+    histVect.at(hI)->GetYaxis()->CenterTitle();
+  }
+
+  Double_t maxValMu = -1;
+  Double_t maxValSig = -1;
+
+  for(unsigned int hI = 0; hI < histVect.size(); ++hI){
+    for(Int_t bIX = 0; bIX < histVect.at(hI)->GetNbinsX(); ++bIX){
+      const std::string centStr = std::to_string((int)centBins[bIX]) + "-" + std::to_string((int)centBins[bIX+1]) + "%";
+      
+      for(Int_t bIY = 0; bIY < histVect.at(hI)->GetNbinsY(); ++bIY){
+	const std::string rStr = rStrV.at(bIY);
+	
+	Double_t val = -1;
+	if(typeVal.at(hI).find("mu") != std::string::npos){
+	  val = TMath::Abs(getMuDataMinusMC(centStr, etaVal.at(hI), rStr, flowVal.at(hI)));
+	  if(val > maxValMu) maxValMu = val;
+	}
+	else{
+	  val = TMath::Abs(getSigDataMinusMC(centStr, etaVal.at(hI), rStr, flowVal.at(hI)));
+	  if(val > maxValSig) maxValSig = val;
+	}
+	
+	histVect.at(hI)->SetBinContent(bIX+1, bIY+1, val);
+      }
+    }
+  }
+
+  TDatime* date = new TDatime();
+  std::string dirName1 = "pdfDir/";
+  std::string dirName2 = "pdfDir/" + std::to_string(date->GetDate()) + "/";
+  checkMakeDir(dirName1);
+  checkMakeDir(dirName2);
+
+  TLatex* label_p = new TLatex();
+  label_p->SetNDC();
+  label_p->SetTextFont(43);
+  label_p->SetTextSize(16);
+
+  for(unsigned int hI = 0; hI < histVect.size(); ++hI){
+    TCanvas* canv_p = new TCanvas("temp_p", "", 500, 400);
+    canv_p->SetLeftMargin(canv_p->GetLeftMargin()*1.2);
+    canv_p->SetBottomMargin(canv_p->GetLeftMargin());
+    canv_p->SetTopMargin(canv_p->GetLeftMargin()*3./4.);
+    canv_p->SetRightMargin(canv_p->GetLeftMargin());
+
+    canv_p->cd();
+
+    if(typeVal.at(hI).find("mu") != std::string::npos) histVect.at(hI)->SetMaximum(maxValMu);
+    else histVect.at(hI)->SetMaximum(maxValSig);
+
+    histVect.at(hI)->SetMinimum(0.0);
+
+    histVect.at(hI)->GetYaxis()->SetLabelColor(0);
+    histVect.at(hI)->SetMarkerSize(histVect.at(hI)->GetMarkerSize()*4./5.);
+    histVect.at(hI)->SetTickLength(0.0, "xyz");
+    histVect.at(hI)->DrawCopy("COLZ TEXT");
+    gStyle->SetOptStat(0);
+    gStyle->SetPaintTextFormat("1.3f");
+
+    label_p->DrawLatex(.15, .95, ("|#Delta#" + typeVal.at(hI) + "|_{Data,MC}, " + etaLabel.at(hI) + ", " + flowLabel.at(hI)).c_str());
+
+    label_p->SetNDC(0);
+    label_p->SetTextSize(14);
+    
+    for(Int_t rI = 0; rI < nRBins; ++rI){
+      label_p->DrawLatex(-4, rBins2[rI]-.025, prettyString(rBins2[rI], 1, false).c_str());
+    }
+
+    label_p->SetNDC();
+    label_p->SetTextSize(16);
+
+
+    std::string saveName = histVect.at(hI)->GetName();
+    saveName = dirName2 + saveName + "_" + std::to_string(date->GetDate()) + ".pdf";
+    canv_p->SaveAs(saveName.c_str());
+    delete canv_p;
+  }
+
+  for(unsigned int hI = 0; hI < histVect.size(); ++hI){
+    delete histVect.at(hI);
+  }
+  histVect.clear();
+  delete date;
+  delete label_p;
+
+  std::cout << "Error maps complete." << std::endl;
+
+  return;
+}
 
 #endif
